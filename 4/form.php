bd074c99396ed11f6d479d1a7740420ffdb19ef9
kuternega <90458770+kuternega@users.noreply.github.com>
2022-04-26 09:08:41 +0300

    Update form.php

--- 4/form.php
+++ 4/form.php
@@ -23,37 +23,37 @@
         <input name="birth" <?php if ($errors['birth']) {print 'class="error"';} ?> value="<?php print $values['birth']; ?>" type="date" />
       </label><br />
      Пол:<br />
-	 <div <?php if ($errors['pol']) {print 'class="error"';} ?>>
-      <label><input type="radio" name="pol" value="m" <?php if($values[pol]=='m') {print 'checked="checked"';} ?> />
+   <div <?php if ($errors['pol']) {print 'class="error"';} ?>>
+      <label><input type="radio" name="pol" value="m" <?php if($values['pol']=='m') {print 'checked="checked"';} ?> />
         М</label>
-      <label><input type="radio" name="pol" value="w" <?php if($values[pol]=='w') {print 'checked="checked"';} ?> />
+      <label><input type="radio" name="pol" value="w" <?php if($values['pol']=='w') {print 'checked="checked"';} ?> />
         Ж</label>
-	  </div><br />
+    </div><br />
         Количество конечностей:<br />
-	  <div <?php if ($errors['konechnosti']) {print 'class="error"';} ?>>
-      <label><input type="radio" name="konechnosti" value="1" <?php if($values[konechnosti]==1) {print 'checked="checked"';} ?> />
+    <div <?php if ($errors['konechnosti']) {print 'class="error"';} ?>>
+      <label><input type="radio" name="konechnosti" value="1" <?php if($values['konechnosti']==1) {print 'checked="checked"';} ?> />
         1</label>
-      <label><input type="radio" name="konechnosti" value="2"  <?php if($values[konechnosti]==2) {print 'checked="checked"';} ?> />
+      <label><input type="radio" name="konechnosti" value="2"  <?php if($values['konechnosti']==2) {print 'checked="checked"';} ?> />
         2</label><br />
-      <label><input type="radio" name="konechnosti" value="3" <?php if($values[konechnosti]==3) {print 'checked="checked"';} ?> />
+      <label><input type="radio" name="konechnosti" value="3" <?php if($values['konechnosti']==3) {print 'checked="checked"';} ?> />
         3</label>
-      <label><input type="radio" name="konechnosti" value="4" <?php if($values[konechnosti]==4) {print 'checked="checked"';} ?> />
+      <label><input type="radio" name="konechnosti" value="4" <?php if($values['konechnosti']==4) {print 'checked="checked"';} ?> />
         4</label><br /> 
-      </div>		
+      </div>    
       <label>
         Сверхспособности:
         <br />
         <select name="powers[]" multiple="multiple" <?php if ($errors['powers']) {print 'class="error"';} ?>>
-          <option value="1" selected="selected">Бессмертие</option>
-          <option value="2">Прохождение сквозь стены </option>
-          <option value="3">Левитация </option>
+          <option value="1" <?php if($errors['powers'] && $values['powers'][1]) {print 'selected="selected"';} ?> >Бессмертие</option>
+          <option value="2" <?php if($errors['powers'] && $values['powers'][2]) {print 'selected="selected"';} ?> >Прохождение сквозь стены </option>
+          <option value="3" <?php if($errors['powers'] && $values['powers'][3]) {print 'selected="selected"';} ?> >Левитация </option>
         </select>
       </label><br />
       <label>
         Биография:<br />
         <textarea name="biography" <?php if ($errors['biography']) {print 'class="error"';} ?> value="<?php print $values['biography']; ?>"></textarea>
       </label><br />
-      <label><input type="checkbox" name="check" <?php if ($errors['check']) {print 'class="error"';} if(!empty($values[check])) {print 'checked="checked"';} ?> />
+      <label <?php if ($errors['check']) {print 'class="error"';} ?>><input type="checkbox" name="check" <?php if(!$errors['check']) {print 'checked="checked"';} ?> />
       С контрактом ознакомлен (а)</label><br />
       <input type="submit" value="Отправить" />
     </form>
